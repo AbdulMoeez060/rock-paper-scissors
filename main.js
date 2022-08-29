863f789c1cf0d0d496a9bc66503b734ba4853852
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-08-29 20:22:44 +0500

    Add simple game logic

--- main.js
+++ main.js
@@ -5,4 +5,32 @@ const getComputerChoice = ()=>{
     return values[choice];
 }
 
-console.log(getComputerChoice())
+const playRound = (playerSelection,computerSelection)=>{
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    computerSelection = computerSelection.toLowerCase();
+    if(playerSelection===computerSelection){
+        alert(`Both are ${playerSelection}, so it's a tie.`);
+    }
+    else if ((playerSelection=='paper' && computerSelection=='rock')|| (playerSelection=='rock' && computerSelection=='scissors')||(playerSelection=='scissors' && computerSelection=='paper') ) {
+        alert(`Player choose ${playerSelection} and Computer choose ${computerSelection}, so Player Wins`);
+    }
+    else if ((playerSelection=='rock' && computerSelection=='paper')|| (playerSelection=='scissors' && computerSelection=='rock')||(playerSelection=='paper' && computerSelection=='scissors') ) {
+        alert(`Player choose ${playerSelection} and Computer choose ${computerSelection}, so Computer Wins`);
+    }
+    else{
+        alert("Wrong input")
+    }
+
+}
+
+const game = ()=>{
+   for (let i = 0; i < 5; i++) {
+        const pChoice = prompt("Please enter Your choice:")
+       console.log(playRound(pChoice,getComputerChoice()))
+    
+   } 
+}
+
+game()
